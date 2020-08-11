53e48d2c2e53aa6358a34d5657f4acd8fd767990
Your Name <you@example.com>
2020-08-11 13:07:38 +0800

    代码

---
+++ leetcode/main453.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	r := minMoves([]int{1, 2, 3})
+	fmt.Println(r)
+}
+
+func minMoves(nums []int) int {
+	l := len(nums)
+	if l <= 1 {
+		return 0
+	}
+	sort.Ints(nums)
+	count := 0
+	for i := 1; i < l; i++ {
+		count += nums[i] - nums[0]
+	}
+	return count
+}
